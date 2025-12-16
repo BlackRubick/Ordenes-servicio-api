10e78322adb802a575798261519b7ed7a5336d24
BlackRubick <blackrubick14@gmail.com>
2025-12-16 13:27:44 -0600

    error update

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getUsers, findByUid, remove, updateUserByUid } = require('../controllers/usersController')
+const { getUsers, findByUid, remove, update } = require('../controllers/usersController')
 const auth = require('../middleware/auth')
 
 router.get('/', auth, getUsers)
 router.get('/:uid', auth, findByUid)
-router.put('/:uid', auth, updateUserByUid)
+router.put('/:uid', auth, update)
 router.delete('/:uid', auth, remove)
 
 module.exports = router
